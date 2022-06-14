d62e077bff3cdf7840f5b44e35fd6bc542d5b958
Aaron <aaron.leon21@gmail.com>
2022-06-14 02:09:33 -0400

    Search bar spacing fixes

--- components/HomeSearchBar.tsx
+++ components/HomeSearchBar.tsx
@@ -47,50 +47,52 @@ const HomeSearchBar = () => {
       noValidate
       id="pet-search"
       autoComplete="off"
-      className="text-center absolute bottom-60 md:bottom-44 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 md:w-1/3  bg-white rounded-md flex justify-between items-center h-16 px-2"
+      className="text-center absolute bottom-60 md:bottom-44 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5/6 sm:w-3/4 lg:w-2/3 xl:w-[40%] bg-white rounded-md flex justify-between items-center h-16 px-2"
     >
       <input
         type="text"
         name="zipcode"
         value={zip}
         onChange={handleZip}
-        className="w-2/3  ml-4 h-10 focus:outline-none border-r-2"
+        className="w-1/4 sm:1/2 md:w-1/2 lg:1/3 ml-1 md:ml-4 h-10 focus:outline-none border-r-2"
         placeholder="Zip Code*"
       />
-      <div onChange={handlePet} className="flex px-4 w-1/3">
-        <div className="mr-5">
+      <div
+        onChange={handlePet}
+        className="flex md:px-4 md:w-1/3 justify-between items-center"
+      >
+        <div className=" flex items-center mr-8 md:mr-2">
           <input
             type="radio"
             value="dog"
             name="pet"
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:outline-none"
+            className="w-4 h-4  text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:outline-none cursor-pointer"
           />
-          <label className="ml-2 text-sm font-medium text-gray-900 ">Dog</label>
+          <label className="ml-2 text-sm  text-gray-900 ">Dog</label>
         </div>
-        <div>
+        <div className="flex items-center">
           <input
             type="radio"
             value="cat"
             name="pet"
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500   focus:outline-none"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:outline-none  cursor-pointer"
           />
-
-          <label className="ml-2 text-sm font-medium text-gray-900 ">Cat</label>
+          <label className="ml-2 text-sm  text-gray-900 ">Cat</label>
         </div>
       </div>
 
       <button
         type="submit"
         onClick={handleSubmit}
-        className="ml-4 bg-blue-500 h-10 w-10 rounded-lg text-white mr-1 "
+        className="ml-1 md:ml-4 bg-blue-500 h-10 w-12 md:w-10 rounded-lg text-white mr-1 "
       >
         {zip?.length === 5 && token && isValidZip(zip) ? (
           <Link href={"/pets"}>
-            <i className="fa-solid fa-magnifying-glass w-full h-full py-3"></i>
+            <i className="fa-solid fa-magnifying-glass w-full h-full  pt-3"></i>
           </Link>
         ) : (
           <Link href={"/"}>
-            <i className="fa-solid fa-magnifying-glass text-lg w-full h-full"></i>
+            <i className="fa-solid fa-magnifying-glass w-full h-full  pt-3"></i>
           </Link>
         )}
       </button>
